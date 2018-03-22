0c0a97a1e03c34548b076cd68983335cf03618cf
Daniel <dan.laframb@gmail.com>
2018-03-22 12:03:01 -0400

    First commit

---
+++ querry.sql
@@ -0,0 +1,43 @@
+-- 1. Lister les le numéro et nom des cliniques, leur adresse et leur gestionnaire, ordonnés par le numéro de clinique 
+
+
+-- 2. Lister les noms des animaux sans doublons dans toutes les cliniques 
+
+
+-- 3. Lister les numéros et noms des propriétaires d’animaux ainsi que les détails de leurs animaux dans une clinique donnée (à vous de la choisir) 
+
+
+-- 4. Lister l’ensemble des examens d’un animal donné
+
+
+-- 5. Lister le détail des traitements d’un animal suite à un examen donné 
+
+
+-- 6. Lister le salaire total des employés par clinique ordonné par numéro de clinique
+
+
+-- 7. Lister le nombre total d’animaux d’un type donné (vous pouvez le choisir) dans chaque clinique 
+
+
+-- 8. Lister le coût minimum, maximum et moyen des traitements
+
+
+-- 9. Quels sont les noms des employés de plus de 50 ans ordonnés par nom ?
+
+
+-- 10. Quels sont les propriétaires dont le nom contient « blay » ? 
+
+
+-- 11. Supprimez le vétérinaire « Jean Tremblay » 
+
+
+-- 12. Lister les détails des propriétaires qui ont un chat et un chien
+
+
+-- 13. Lister les détails des propriétaires qui ont un chat ou un chien
+
+
+-- 14. Lister les détails des propriétaires qui ont un chat mais pas de chien vacciné contre la grippe
+-- (la condition vacciné contre la grippe ne s’applique qu’au chien) 
+-- 
+-- 
