3eacbdc8cee5b9e9308020540c89daca670267a3
Daniel <dan.laframb@gmail.com>
2018-03-29 16:29:03 -0400

    data for owners

--- data.sql
+++ data.sql
@@ -12,18 +12,25 @@ VALUES
 ("Norm", "Auger", "245 Longpoint Crescent, Ottawa, Ontario, H0B 3M1", "613-938-2937", "1966-12-15", "M", "123-6346-127", "Veterinaire", 78000.00, 1),
 ("Chantal", "Richardson", "123 Hatchet Road, Ottawa, Ontario, B4W 9I1", "613-213-5463", "1963-08-22", "F", "083-2816-998", "Veterinaire", 85300.23, 1),
 ("Judy", "Middlemiss", "156 Mercedes Lane, Ottawa, Ontario, J5K 9L0", "613-163-1122", "1979-01-05", "F", "193-8274-981", "Gestionnaire", 53000.12, 1),
-
 ("Julia", "Toomey", "1877 Moonlight Road, Ottawa, Ontario, C0V 9A1", "613-526-1553", "1992-03-24", "F", "441-7722-573", "Administration", 39000.12, 2),
 ("Kara", "Webb", "288 Canine Avenue, Ottawa, Ontario, L1L 1D5", "613-223-5514", "1989-05-23", "F", "124-5553-125", "Veterinaire", 88500.02, 2),
 ("Samantha", "Briggs", "233 Richards Road, Ottawa, Ontario, B8B 2G5", "613-155-5663", "1990-11-15", "F", "146-2256-120", "Veterinaire", 95000.65, 2),
 ("Rich", "Fronning", "156 Woodland Circle, Ottawa, Ontario, N8Q 0T4", "613-778-3352", "1979-01-31", "M", "525-5553-167", "Gestionnaire", 62050.50, 2),
-
 ("Alexandra", "Clearview", "5 Thorn Lane, Ottawa, Ontario, N2C 9K5", "613-111-4624", "1985-10-14", "F", "124-1526-122", "Administration", 50500.52, 3),
 ("Bernard", "Poukay", "33 Linda Lane, Ottawa, Ontario, K2G 5T6", "613-364-3666", "1986-09-09", "M", "414-3958-395", "Veterinaire", 90100.80, 3),
 ("Bonnie", "Prosser", "776 Washignton Street, Ottawa, Ontario, M59 1L4", "613-869-2803", "1964-08-11", "F", "143-8958-362", "Veterinaire", 86700.65, 3),
 ("Matthew", "Fraser", "209 Parkdale Crescent, Ottawa, Ontario, O0A 9D2", "613-325-6623", "1992-07-24", "M", "424-5266-278", "Gestionnaire", 51090.75, 3),
-
 ("Adam", "Glover", "99 Gretzky Road, Ottawa, Ontario, F4G 8N1", "613-124-4422", "1994-02-24", "M", "634-2366-253", "Administration", 38000.02, 4),
 ("Leo", "DiGenard", "299 Vermont Street, Ottawa, Ontario, Z7F 1H4", "613-256-3278", "1983-04-20", "M", "235-5039-309", "Veterinaire", 79010.98, 4),
 ("Mary", "Hernandez", "1 Main Street, Ottawa, Ontario, Z5T 1B2", "613-398-1263", "1954-08-11", "F", "234-5233-520", "Veterinaire", 102000.15, 4),
-("Emilie", "Hooly", "15 Ritchcraft Road, Ottawa, Ontario, F2P 0M2", "613-325-6623", "1988-06-14", "F", "356-2099-102", "Gestionnaire", 49500.85, 4),
+("Emilie", "Hooly", "15 Ritchcraft Road, Ottawa, Ontario, F2P 0M2", "613-325-6623", "1988-06-14", "F", "356-2099-102", "Gestionnaire", 49500.85, 4);
+
+INSERT INTO owners(fName, lName, address, telephone)
+VALUES
+("Troy", "Wilfrid", "218 Slane Avenue, Ottawa, Ontario, L3B 8Y6", "613-928-2889"),
+("Janet", "Levignston", "100 Trudeau Read, Ottawa, Ontario, Z3A 6S2", "613-112-4278"),
+("Michael", "Moore", "9 Picket Lane, Ottawa, Ontario, P1V 3E3", "613-144-2140"),
+("Tobias", "Henderson", "199 Covert Street, Ottawa, Ontario, J2R 0Q2", "613-028-2984"),
+("Kelly", "Wendle", "92 Mitchem Road, Ottawa, Ontario, D8G 2H8", "613-125-52566"),
+("Ryan", "Scott", "144 Busser Crescent, Ottawa, Ontario, B9O 1Q3", "613-146-2208"),
+("James", "Bryant", "155 Huff Road, Ottawa, Ontario, D5E 0W7", "613-187-1993");
