48275bb8857a08977cef35e214f0de14e06c1e41
Patrick <kizpatrick.KP@gmail.com>
2018-04-11 18:50:34 -0400

    updated query

--- querry.sql
+++ querry.sql
@@ -1,12 +1,16 @@
 -- 1. Lister les le numéro et nom des cliniques, leur adresse et leur gestionnaire, ordonnés par le numéro de clinique 
-
+SELECT cliniquesNum, Name, address, gestionnaireId
+     FROM cliniques
+     ORDER BY cliniqueNom
 
 -- 2. Lister les noms des animaux sans doublons dans toutes les cliniques 
 SELECT DISTINCT name 
 	from animals;
 
 -- 3. Lister les numéros et noms des propriétaires d’animaux ainsi que les détails de leurs animaux dans une clinique donnée (à vous de la choisir) 
-
+SELECT O.ownerNum, O.fname, O.lname, A*
+       FROM owner O, animaux A 
+       WHERE O.ownerNum = A.ownerNum
 
 -- 4. Lister l’ensemble des examens d’un animal donné
 SELECT * 
@@ -14,7 +18,8 @@ SELECT *
 	WHERE animalNo = 3;
 
 -- 5. Lister le détail des traitements d’un animal suite à un examen donné 
-
+SELECT treatmentId, examId
+       FROM results 
 
 -- 6. Lister le salaire total des employés par clinique ordonné par numéro de clinique
 SELECT SUM(salary) 
@@ -22,7 +27,10 @@ SELECT SUM(salary)
 	WHERE cliniqueNum = 1;
 
 -- 7. Lister le nombre total d’animaux d’un type donné (vous pouvez le choisir) dans chaque clinique 
-
+SELECT SUM(*)
+       FROM Animals
+       WHERE type LIKE '%dog%'
+       GROUP BY cliniqueNum
 
 -- 8. Lister le coût minimum, maximum et moyen des traitements
 SELECT 
@@ -32,7 +40,9 @@ SELECT
 	FROM results;
 
 -- 9. Quels sont les noms des employés de plus de 50 ans ordonnés par nom ?
-
+SELECT fname, lname
+     FROM employees
+     WHERE age>50
 
 -- 10. Quels sont les propriétaires dont le nom contient « blay » ? 
 SELECT *
@@ -41,7 +51,8 @@ SELECT *
 
 -- 11. Supprimez le vétérinaire « Jean Tremblay » 
 
-
+DELETE FROM employee
+       WHERE (fname='Jean' AND lname='Tremblay') AND fonction='veterinaire'
 -- 12. Lister les détails des propriétaires qui ont un chat et un chien
 SELECT *
 	FROM owners
@@ -49,6 +60,10 @@ SELECT *
 	WHERE animals.type LIKE '%dog%' AND animals.type LIKE '%cat%';
 
 -- 13. Lister les détails des propriétaires qui ont un chat ou un chien
+SELECT *
+	FROM owners
+	INNER JOIN animals ON owners.ownerNum = animals.ownerNum
+	WHERE animals.type LIKE '%dog%' OR animals.type LIKE '%cat%';
 
 
 -- 14. Lister les détails des propriétaires qui ont un chat mais pas de chien vacciné contre la grippe
@@ -65,4 +80,10 @@ SELECT *
 		WHERE results.treatmentId = T112 AND animal.type LIKE '%cat%';
 	);
 
-
+--15.Lister tous les animaux d’une clinique donnée avec leurs traitements s’ils existent. Dans le cas contraire, affichez null.
+SELECT
+      FROM animal
+      WHERE animalNo IN
+      (SELECT r.animalNO, t.treatmentId
+          FROM results r RIGHT JOIN  treatment t
+          ON r.treatmentId = t.treatmnetId)
